a68417ed3ddd92fd393432817611f44933e7dd74
ldc-369 <ldc369tpqt@gmail.com>
2025-04-19 22:35:47 +0700

    commit

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
-import tensorflow as tf
+from tensorflow import expand_dims, convert_to_tensor
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 
 
 app = Flask(__name__)
 
-BATCH_SIZE = 64
-IMAGE_SIZE = 255
+BATCH_SIZE = 32
+IMAGE_SIZE = 224
 CHANNEL = 3
-EPOCHS = 20
 
 labels = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
-model = tf.keras.models.load_model('./crop-disease-classification/model/model.h5')
+model = load_model('./crop-disease-classification/model/model.h5')
 
 def validate_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
 
-def prepare_input(img_tf):
-    # C1:
-    # img = tf.keras.utils.load_img("path/to/image.jpg")
-    # img = tf.keras.utils.img_to_array(img)  # thành mảng NumPy
-    # image_tf = tf.convert_to_tensor(image_np)  # image_np: img dạng mảng
-    # image_tf = tf.image.convert_image_dtype(image_tf, tf.float32)
-    # img = tf.expand_dims(img, axis=0)       # thêm batch dimension: (1, H, W, C)
+def prepare_input(filepath):
+    img_PIL = load_img(filepath, target_size=(IMAGE_SIZE, IMAGE_SIZE))  # load and resize
     
-    img_tf = tf.keras.preprocessing.image.img_to_array(img_tf)
-    print(img_tf.shape)
+    # img_tf = tf.image.convert_image_dtype(img_tf, tf.float32)   # convert_image_dtype() tư động scale
+    img_arr = img_to_array(img_PIL)   # 3D (h, w, rgb)
     
-    # chuyển sang 4D (batch_size, h, w, rgb) (cấu trúc 1 batch cho đầu vào model và scaler)
-    F_predict = tf.expand_dims(img_tf, 0)
+    img_arr = expand_dims(img_arr, axis=0)  # 4D (batch_size, h, w, rgb)
+    F_predict = convert_to_tensor(img_arr)
     
     return F_predict
 
@@ -60,19 +56,14 @@ def index():
             filename = secure_filename(file.filename)
             filepath = os.path.join('crop-disease-classification\\static', filename)
             
-            file.save(filepath)  # lưu file
+            file.save(filepath)
 
-            # Read the image
-            img_tf = tf.keras.preprocessing.image.load_img(filepath, target_size=(IMAGE_SIZE, IMAGE_SIZE))
-            
-            F_predict = prepare_input(img_tf)
-
+            F_predict = prepare_input(filepath)
             predicted_label, predicted_score = predict(F_predict)
-            print(filepath)
-
+            
             return render_template('index.html', image_path=f"static/{filename}", actual_label=predicted_label, predicted_label=predicted_label, predicted_score=predicted_score)
     
-    return render_template("index.html")  # render trang index.html
+    return render_template("index.html")  # render index.html
 
 if __name__ == '__main__':
     app.run(debug=True)
